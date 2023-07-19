c8eea28e4c310f3806caee6f942d338c2d6ab61b
timo9939 <timothylau20199@gmail.com>
2023-07-19 22:50:29 +0100

    login password protect display able and disable eye works

--- italian.js
+++ italian.js
@@ -1,5 +1,9 @@
+
+
 const formOpenBtn=document.querySelector("#form_open"),
 formCloseBtn=document.querySelector("#closeFormBtn"),
+pwProtectOn=document.querySelector(".fa-eye-slash"),
+pwProtectOff=document.querySelector(".fa-eye"),
 home=document.querySelector(".home");
 
 function addShow(){
@@ -10,6 +14,41 @@ function closeForm(){
     home.classList.remove('show')
 }
 
+function displayPw(){
+    console.log("pw protect disabled")
+    let getPwInput=document.querySelector(".inputPassword")
+    if(getPwInput.type==="password"){
+    getPwInput.type="text"
+}   
+else{
+    getPwInput.type="password"
+}
+ pwProtectOn.style.opacity=0
+
+    pwProtectOff.style.opacity=1
+    pwProtectOn.style.pointerEvents='none'
+    pwProtectOff.style.pointerEvents='auto'
+
+}
+
+function hidePw(){
+    console.log("pw protect able")
+    let getPwInput=document.querySelector(".inputPassword")
+    if(getPwInput.type==="password"){
+    getPwInput.type="text"
+}   
+else{
+    getPwInput.type="password"
+}
+
+    pwProtectOff.style.opacity=0
+    pwProtectOn.style.opacity=1
+    pwProtectOff.style.pointerEvents='none'
+    pwProtectOn.style.pointerEvents='auto'
+}
+
 formOpenBtn.addEventListener('click',addShow);
 formCloseBtn.addEventListener('click',closeForm);
 
+pwProtectOn.addEventListener('click',displayPw)
+pwProtectOff.addEventListener('click',hidePw)
