f3b06b4041c96f30bf311885344b3c9b89a6a1e2
timo9939 <timothylau20199@gmail.com>
2023-07-19 19:49:13 +0100

    display login page button work

--- italian.js
+++ italian.js
@@ -1,1 +1,15 @@
-console.log("js is working")
+const formOpenBtn=document.querySelector("#form_open"),
+formCloseBtn=document.querySelector("#closeFormBtn"),
+home=document.querySelector(".home");
+
+function addShow(){
+home.classList.add("show")
+}
+
+function closeForm(){
+    home.classList.remove('show')
+}
+
+formOpenBtn.addEventListener('click',addShow);
+formCloseBtn.addEventListener('click',closeForm);
+
