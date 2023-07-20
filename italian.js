ed5f3cff75cdd1f2f7e59c9928663a5ba251abb1
timo9939 <timothylau20199@gmail.com>
2023-07-20 11:45:53 +0100

    Adding the comment in js file

--- italian.js
+++ italian.js
@@ -4,16 +4,21 @@ const formOpenBtn=document.querySelector("#form_open"),
 formCloseBtn=document.querySelector("#closeFormBtn"),
 pwProtectOn=document.querySelector(".fa-eye-slash"),
 pwProtectOff=document.querySelector(".fa-eye"),
-home=document.querySelector(".home");
+home=document.querySelector(".home"),
+signUpOpenBtn=document.getElementById("SignUp"),
+signUpForm=document.querySelector(".form.signUp_form"),
+logInForm=document.querySelector(".form.login_form");
 
+// Showing the login page by pressing the login button at the nav bar
 function addShow(){
 home.classList.add("show")
 }
-
+// Close the login Form by clicking the cross symbol
 function closeForm(){
     home.classList.remove('show')
 }
 
+//Disable the password protection by clicking the eye symbol
 function displayPw(){
     console.log("pw protect disabled")
     let getPwInput=document.querySelector(".inputPassword")
@@ -31,6 +36,7 @@ else{
 
 }
 
+//Enable the password protection by clicking the eye symbol
 function hidePw(){
     console.log("pw protect able")
     let getPwInput=document.querySelector(".inputPassword")
@@ -47,8 +53,18 @@ else{
     pwProtectOn.style.pointerEvents='auto'
 }
 
+// Display the signup Form page by clicking the "Don't have an account? Sign up"
+function displaySignUpForm(){
+ 
+    logInForm.style.display='none'
+    signUpForm.classList.add("show")
+   console.log("show signup form")
+}
+
 formOpenBtn.addEventListener('click',addShow);
 formCloseBtn.addEventListener('click',closeForm);
 
 pwProtectOn.addEventListener('click',displayPw)
 pwProtectOff.addEventListener('click',hidePw)
+
+signUpOpenBtn.addEventListener('click',displaySignUpForm)
